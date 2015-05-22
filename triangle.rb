02b92d490438b69c453e3be0c70e3c11cc012dc3
neudabei <rschultzejena@gmail.com>
2015-05-22 10:02:00 +0100

    Further progress

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  
+  #conditions that make triangle impossible 0, 0, 0 || 3, 4, -5 || 1, 1, 3 || 2, 4, 2
+  raise TriangleError, "This can't be a triangle" if a == 0 && b == 0 && c == 0
+  raise TriangleError, "This can't be a triangle" if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError, "This can't be a triangle" if (a + b) <= c || (a + c) <= b || (c + b) <= a
+
+  return :equilateral if a == b && b == c
+  return :isosceles if (a == b && b != c) || (a != b && b == c) || (a != b && a == c)
+  return :scalene if a != b && b != c && a != c
+     
 end
 
 # Error class used in part 2.  No need to change this code.
